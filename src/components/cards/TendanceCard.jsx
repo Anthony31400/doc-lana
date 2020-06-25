c8909cc5f41ee0d1cc77c297b419e10947811e63
Alexandra31150 <alexandrap44@gmail.com>
2020-06-26 00:41:55 +0200

    ajout lien voyage

--- src/components/cards/TendanceCard.jsx
+++ src/components/cards/TendanceCard.jsx
@@ -38,7 +38,13 @@ class TendanceCard extends Component {
         <hr></hr>
         <div className="voyage">
           <h3>Reserver votre séjour</h3>
-          <p>Vous avez trouvé votre </p>
+          <p>Vous avez trouvé votre opération ?</p>
+          <a
+            href="https://www.liligo.fr/?utm_source=google&utm_medium=cpc&utm_campaign=G-L-FR-BD-Brand-SEA-PC-L&utm_term=liligo&k_clickid=_k_CjwKCAjwltH3BRB6EiwAhj0IUN6K69SbR3Nnsbo3ZIZ74_0otbNvzmtoQkbIVfBX7wSn8NVe8H0rBBoCDgkQAvD_BwE_k_&gclid=CjwKCAjwltH3BRB6EiwAhj0IUN6K69SbR3Nnsbo3ZIZ74_0otbNvzmtoQkbIVfBX7wSn8NVe8H0rBBoCDgkQAvD_BwE"
+            target="_blank"
+          >
+            Cliquez ici
+          </a>
         </div>
       </div>
     );

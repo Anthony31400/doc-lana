98ec500f383f6d97bfd402971dc8e13cf18a7c03
Alexandra31150 <alexandrap44@gmail.com>
2020-06-26 00:06:17 +0200

    ajout de select pour trouver chirurgien

--- src/components/cards/TendanceCard.jsx
+++ src/components/cards/TendanceCard.jsx
@@ -8,12 +8,28 @@ class TendanceCard extends Component {
   }
 
   render() {
-    const { name, operation, price } = this.props;
     return (
-      <div className="display-country">
-        <p className="display-county-name">{name}</p>
-        <p className="display-county-operation">{operation}</p>
-        <p className="display-county-price">{price}</p>
+      <div className="tendance">
+        <div className="chirurgien">
+          <p>Trouver votre chirurgien</p>
+        </div>
+        <select name="specialites" id="spec-select">
+          <option value="">--Choisir une spécialité--</option>
+          <option value="lipo">Liposuccion</option>
+          <option value="abdo">Abdominoplastie</option>
+          <option value="seins">Chirurgie des seins</option>
+          <option value="visage">Chirurgie du visage</option>
+        </select>
+        <select name="pays" id="pays-select">
+          <option value="">--Choisir un pays--</option>
+          <option value="france">France</option>
+          <option value="israel">Israël</option>
+          <option value="turquie">Turquie</option>
+          <option value="mexique">Mexique</option>
+          <option value="maroc">Maroc</option>
+          <option value="roumanie">Roumanie</option>
+          <option value="pays-bas">Pays-Bas</option>
+        </select>
       </div>
     );
   }

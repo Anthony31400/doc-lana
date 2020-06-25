a2aa2b1b84ee1573ef017e4b924e5e13fb93f6a9
Anthony <anthony.gensane@gmail.com>
2020-06-25 23:15:29 +0200

    suppression comment

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -31,16 +31,6 @@ class NewsCard extends React.Component {
     this.displayArticle = this.displayArticle.bind(this);
   }
 
-  displayArticle(article) {
-    for (let i = 0; i < article.length; i++) {
-      let { info } = this.article[i];
-      setTimeout(() => {
-        return <img src={info.src} alt={info.id} />;
-      }, i * 1000);
-      i += 1;
-    }
-  }
-
   render() {
     return (
       <div id="slider">
@@ -55,24 +45,3 @@ class NewsCard extends React.Component {
 }
 
 export default NewsCard;
-/*
-      <div id="slider">
-        <div id="slides">
-          <figure id="slide1">
-            <img src="https://zupimages.net/up/20/26/8i42.png" alt="actu1" />
-          </figure>
-          <figure id="slide2">
-            <img src="https://zupimages.net/up/20/26/tctv.png" alt="actu2" />
-          </figure>
-          <figure id="slide3">
-            <img src="https://zupimages.net/up/20/26/zv3o.png" alt="actu3" />
-          </figure>
-          <figure id="slide4">
-            <img src="https://zupimages.net/up/20/26/eus5.png" alt="actu4" />
-          </figure>
-          <figure id="slide5">
-            <img src="https://zupimages.net/up/20/26/od8h.png" alt="actu5" />
-          </figure>
-        </div>
-      </div>
-*/

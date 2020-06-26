cd874b259a692a5710769a38eb202a5634f4f7f9
Leslie <lesliec38@hotmail.fr>
2020-06-26 11:47:42 +0200

    final touche accueil

--- src/components/navigation/Accueil.jsx
+++ src/components/navigation/Accueil.jsx
@@ -3,17 +3,25 @@ import './Accueil.css';
 
 const Accueil = () => {
   return (
-    <div>
+    <div className="AccueilDisplay">
       <h1 className="title-acc">Accueil</h1>
-      <p className="text-acc">
-        Suivez l'actualité de la bourse de la chirurgie en direct : Cours des opérations, tarifs,
-        actualités ...
-      </p>
-      <img src="https://zupimages.net/up/20/26/q2lw.jpg" alt="chirurgien" className="img-accueil" />
-      <p className="text-acc">
-        L’idéal de beauté reflète un instinct primitif ; la recherche de beauté passe souvent avant
-        l’instinct de conservation, la peur de la laideur dépasse la peur de la mort.
-      </p>
+      <div className="Accueil">
+        <div className="Text">
+          <h2 className="text-acc">
+            Suivez l'actualité de la bourse chirurgicale en direct : Cours des opérations, tarifs,
+            destinations phares ...
+          </h2>
+          <blockquote>
+            <p className="text-acc">
+              <cite>
+                " L’idéal de beauté reflète un instinct primitif : la recherche de la beauté passe
+                souvent avant l’instinct de conservation, la peur de la laideur dépasse la peur de
+                la mort. "
+              </cite>
+            </p>
+          </blockquote>
+        </div>
+      </div>
     </div>
   );
 };

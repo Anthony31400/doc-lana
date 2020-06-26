6fc751b017b95755fddb2c1da315b644b8b4c91f
Alexandra31150 <alexandrap44@gmail.com>
2020-06-26 10:28:32 +0200

    ajout image accueil

--- src/components/navigation/Accueil.jsx
+++ src/components/navigation/Accueil.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Accueil.css';
 
 const Accueil = () => {
   return (
     <div>
-      <h1>acceuil</h1>
+      <h1>Accueil</h1>
+      <img src="https://zupimages.net/up/20/26/q2lw.jpg" alt="chirurgien" className="img-accueil" />
     </div>
   );
 };
